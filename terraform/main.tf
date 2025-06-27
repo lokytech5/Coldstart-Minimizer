ddbf7cc8d96451a870dc60fce4ca17d23974210b
lokytech5 <lokytech5@gmail.com>
2025-06-27 19:51:35 +0100

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket        = "demo-bucket-${random_id.suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Name        = "Demo Bucket"
+    Environment = "dev"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
