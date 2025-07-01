c4d16ff12f70bd69b1cbaf8f9bfadddda6c0a917
lokytech5 <lokytech5@gmail.com>
2025-07-01 19:30:05 +0100

    added comment

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#comment here
 resource "aws_s3_bucket" "demo_bucket" {
   bucket        = "demo-bucket-${random_id.suffix.hex}"
   force_destroy = true
