e6ad69f68754207d01ad577a5bc4f1ee7d8af977
lokytech5 <lokytech5@gmail.com>
2025-07-04 02:30:23 +0100

    added backend config to main terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "coldstartminimizer-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "coldStartMinizer-Lock"
+  }
+}
 
 resource "aws_s3_bucket" "demo_bucket" {
   bucket        = "demo-bucket-${random_id.suffix.hex}"
